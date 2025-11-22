ba5c4be73ec3abc49fe2c77f5274060489c77b60
Elizaveta Lim <eliza.volga21@gmail.com>
2025-11-22 15:52:49 +0300

    remove: todo in json lib

--- core/nodes/json.hpp
+++ core/nodes/json.hpp
@@ -11634,7 +11634,7 @@ class binary_reader
     {
         if (get_char)
         {
-            get();  // TODO(niels): may we ignore N here?
+            get();
         }
 
         if (JSON_HEDLEY_UNLIKELY(!unexpect_eof(input_format, "value")))
@@ -21239,10 +21239,10 @@ class basic_json // NOLINT(cppcoreguidelines-special-member-functions,hicpp-spec
     /// @sa https://json.nlohmann.me/api/basic_json/basic_json/
     basic_json(basic_json&& other) noexcept
         : json_base_class_t(std::forward<json_base_class_t>(other)),
-          m_data(std::move(other.m_data)) // cppcheck-suppress[accessForwarded] TODO check
+          m_data(std::move(other.m_data)) // cppcheck-suppress[accessForwarded]
 #if JSON_DIAGNOSTIC_POSITIONS
-        , start_position(other.start_position) // cppcheck-suppress[accessForwarded] TODO check
-        , end_position(other.end_position) // cppcheck-suppress[accessForwarded] TODO check
+        , start_position(other.start_position) // cppcheck-suppress[accessForwarded]
+        , end_position(other.end_position) // cppcheck-suppress[accessForwarded]
 #endif
     {
         // check that passed value is valid
